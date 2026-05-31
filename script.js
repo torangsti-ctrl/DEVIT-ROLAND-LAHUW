2f93c6dc6fe7dc5823bf7d2cd0b79a0292a7fdfb
DEVIT ROLAND LAHUW <devitrolandlahuw@gmail.com>
2026-05-31 13:52:20 +0900

    FILE UPLOAD

---
+++ script.js
@@ -0,0 +1,9 @@
+<script type="javascript">
+var flash = {};
+var params = {mp3: "audio/test.mp3"};
+swfobject.embedSWF(
+"flash/pplayer_mp3_1.0.0.swf",
+"music-player", "20", "8.0.0"
+flashvars
+parameters
+);
